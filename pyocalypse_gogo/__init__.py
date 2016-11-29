e7bfb1814081b5542a3a9625c74f0d19892d8445
Landon Bouma <landon.bouma@gmail.com>
2016-11-29 10:35:15 -0600

    Add alternative to subprocess.check_output: os.execv.

--- pyocalypse_gogo/__init__.py
+++ pyocalypse_gogo/__init__.py
@@ -7,9 +7,20 @@ def main():
     # This feels so dirty it feels so right.
     pyocalypse_path = os.path.dirname(__file__)
     abspath = os.path.join(pyocalypse_path, 'gogo.sh')
-    # Send all arguments (but not script name) to Bash script.
-    #subprocess.call([abspath,] + sys.argv[1:])
-    proj_dir = subprocess.check_output([abspath,] + sys.argv[1:])
-    proj_dir = proj_dir.strip()
-    print(proj_dir.decode())
+    if True:
+        # Send all arguments (but not script name) to Bash script.
+        #subprocess.call([abspath,] + sys.argv[1:])
+        proj_dir = subprocess.check_output([abspath,] + sys.argv[1:])
+        proj_dir = proj_dir.strip()
+        print(proj_dir.decode())
+    else:
+        # NOTE/2016-11-29: The subprocess.check_output happens in a
+        # subshell, so the `source .bashrc-client` doesn't stick.
+        # I tried using os.execv() but was having issues... so for
+        # now just sticking with gogo bash function. You can access
+        # Python route via `pyocalypse-gogo`.
+        print("abspath: %s" % (abspath,))
+        print("sys.argv: %s" % (sys.argv[1:],))
+        sys.stdout.flush()
+        os.execv(abspath, sys.argv[1:])
 
