e0855bd3cb6aeb97d326ebd95d6d2fe02fb72897
Landon Bouma <landon.bouma@gmail.com>
2017-05-04 08:03:32 -0500

    Define Bash vars using 'local' to avoid polluting env namespace.

--- pyocalypse_gogo/gogo.sh
+++ pyocalypse_gogo/gogo.sh
@@ -12,40 +12,59 @@
 #
 #    gogo client-name task-name
 #
-# or some variant thereof. Use <Tab>s to make Life Easier.
+# or some variant thereof.
+#
+# FEATURES:
+#
+#  Use <Tab>s to make Life Easier.
+#
+#    Pressing <Tab> will suggest client and task names.
+#
+#  Bash script sourcing.
+#
+#    gogo sources .bashrc-{{client-name}}, if found invursively;
+#
+#    gogo wires the first {{.exoline}} if finds invursively;
 
 # -------------------------------------------------------------------------
 
 # Walk up the path looking for a file with the matching name.
 invursive_find () {
 
-    FILEPATH=$1
+    local filepath=$1
 
-    if [[ -z ${FILEPATH} ]]; then
+    if [[ -z ${filepath} ]]; then
         echo "ERROR: Please specify a file."
         return 1
     fi
 
-    FILENAME=$(basename ${FILEPATH})
-    DIRPATH=$(dirname ${FILEPATH})
+    local filename=$(basename ${filepath})
+    local dirpath=$(dirname ${filepath})
 
     # Deal only in full paths.
     # Symlinks okay (hence not pwd -P or readlink -f).
-    pushd ${DIRPATH} &> /dev/null
-    DIRPATH=$(pwd)
+    pushd ${dirpath} &> /dev/null
+    dirpath=$(pwd)
     popd &> /dev/null
 
     INVURSIVE_PATH=""
     # We don't return things from file system root. Because safer?
-    while [[ ${DIRPATH} != '/' ]]; do
-        if [[ -f ${DIRPATH}/${FILENAME} ]]; then
-            #echo "DIRPATH: ${DIRPATH}"
-            #echo "FILENAME: ${FILENAME}"
-            INVURSIVE_PATH="${DIRPATH}/${FILENAME}"
+    while [[ ${dirpath} != '/' ]]; do
+        if [[ -f ${dirpath}/${filename} ]]; then
+            #echo "dirpath: ${dirpath}"
+            #echo "filename: ${filename}"
+            INVURSIVE_PATH="${dirpath}/${filename}"
             break
         fi
-        DIRPATH=$(dirname ${DIRPATH})
+        dirpath=$(dirname ${dirpath})
     done
+
+    # Here's how chruby/auto.sh does the same:
+    #   local dir="$PWD/"
+    #   until [[ -z "$dir" ]]; do
+    #       dir="${dir%/*}"
+    #       if ... fi
+    #   done
 
     #echo "INVURSIVE_PATH: ${INVURSIVE_PATH}"
 }
@@ -88,63 +107,83 @@ invursive_find () {
 #   that starts with <client>-.
 
 gogo () {
-    CLIENT=$1
-    SUBDIR=$2
+    local client=$1
+    local subdir=$2
 
-    if [[ -z ${CLIENT} ]]; then
+    if [[ -z ${client} ]]; then
         >&2 echo "USAGE: gogo client subdir"
         return 1
     fi
 
-    CLIENT_DIR="${GOGO_CLIENT_DIR}/${CLIENT}"
+    local client_dir="${GOGO_CLIENT_DIR}/${client}"
 
-    if [[ -n ${SUBDIR} ]]; then
+    local target_dir=""
+    if [[ -n ${subdir} ]]; then
         # You can either specify the subdir name completely, or, if
         # it shares a prefix with the client name, you can skip the
         # prefix. E.g., for "task/task-76", "76" or "task-76" works.
-        TARGET_DIR="${CLIENT_DIR}/${CLIENT}-${SUBDIR}"
-        if [[ ! -d ${TARGET_DIR} ]]; then
-            TARGET_DIR="${CLIENT_DIR}/${SUBDIR}"
-            if [[ ! -d ${TARGET_DIR} ]]; then
-                >&2 echo "ERROR: Could not find ticket dir (${TARGET_DIR}) under: ${CLIENT_DIR}"
+        target_dir="${client_dir}/${client}-${subdir}"
+        if [[ ! -d ${target_dir} ]]; then
+            target_dir="${client_dir}/${subdir}"
+            if [[ ! -d ${target_dir} ]]; then
+                >&2 echo "ERROR: Could not find ticket dir (${target_dir}) under: ${client_dir}"
                 return 1
             fi
         fi
-        CLIENT_DIR=$(dirname ${TARGET_DIR})
+        client_dir=$(dirname ${target_dir})
     else
-        TARGET_DIR="${CLIENT_DIR}"
+        target_dir="${client_dir}"
     fi
 
-    if [[ ! -d ${CLIENT_DIR} ]]; then
-        >&2 echo "ERROR: Could not find client dir at: ${CLIENT_DIR}"
+    if [[ ! -d ${client_dir} ]]; then
+        >&2 echo "ERROR: Could not find client dir at: ${client_dir}"
         return 1
     fi
 
     # Load a bashrc, maybe.
-    invursive_find "${CLIENT_DIR}/.bashrc-${CLIENT}"
+    invursive_find "${target_dir}/.bashrc-${client}"
     if [[ -f ${INVURSIVE_PATH} ]]; then
         source ${INVURSIVE_PATH}
         echo "Sourced ${INVURSIVE_PATH}"
     else
         : # Meh.
-        #echo "No .bashrc under: "${CLIENT_DIR}/.bashrc-${CLIENT}""
+        #echo "No .bashrc under: "${client_dir}/.bashrc-${client}""
         #echo "or at: ${INVURSIVE_PATH}"
     fi
+    unset INVURSIVE_PATH
 
     # Rewire ~/.exoline for the project, maybe.
-    invursive_find "${CLIENT_DIR}/.exoline"
+    invursive_find "${target_dir}/.exoline"
     if [[ -f ${INVURSIVE_PATH} ]]; then
         if [[ ! -h ${HOME}/.exoline ]]; then
-            >&2 echo "OOPS: Your ~/.exoline is not a symlink. Not replacing."
+            >&2 echo "WHOA: Your ~/.exoline is not a symlink. Not replacing."
         else
             /bin/ln -sf ${INVURSIVE_PATH} ${HOME}/.exoline
-            >&2 echo "Symlinked ~/.exoline"
+            >&2 echo -e "- ${HOTPINK}Symlnkd${font_normal_bash} ~/.exoline"
         fi
-    else
-        >&2 echo "Skipping ~/.exoline symlink: no replacement found."
+    #else
+    #    >&2 echo "Skipping ~/.exoline symlink: no replacement found."
     fi
+    unset INVURSIVE_PATH
 
-    pushd ${TARGET_DIR} &> /dev/null
+    # 2017-05-03: Party all the time.
+    # E.g.,
+    #   echo "ruby-2.3" > .ruby-version
+    invursive_find "${target_dir}/.ruby-version"
+    if [[ -f ${INVURSIVE_PATH} ]]; then
+        local ruby_vers=""
+        if { read -r ruby_vers < "${INVURSIVE_PATH}"; } 2>/dev/null; then
+            if [[ -n "${ruby_vers}" ]]; then
+                chruby "${ruby_vers}"
+                >&2 echo -e "- ${HOTPINK}Patched${font_normal_bash} ${ruby_vers}"
+            else
+                >&2 echo "WARNING: .ruby-version specified but empty"
+            fi
+        fi
+    fi
+    unset INVURSIVE_PATH
+
+    pushd ${target_dir} &> /dev/null
 
     echo "Entered ${TARGET_DIR}"
 }
